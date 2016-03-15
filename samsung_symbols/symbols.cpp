9105b86c4ec39dd272cb85aee30a1b55b40af9ef
RGIB <gibellini.roberto@gmail.com>
2016-03-15 17:37:47 +0100

    n5100 : symbols
    Change-Id: I2e38f94a9f01454aab726dc6c9b7bb520b0c36c2

--- samsung_symbols/symbols.cpp
+++ samsung_symbols/symbols.cpp
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2015 The CyanogenMod Project
+ * Copyright (C) 2016 The CyanogenMod Project
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -15,52 +15,13 @@
  */
 
 #include <stdlib.h>
-#include <malloc.h>
-
-/***********************************************************************************/
-// LIBRIL //
-/***********************************************************************************/
 
 // android::Parcel::writeString16(unsigned short const*, unsigned int)
-extern "C" int _ZN7android6Parcel13writeString16EPKtj();
+extern "C" int _ZN7android6Parcel13writeString16EPKDsj();
+
 extern "C" int _ZN7android6Parcel13writeString16EPKtj(){
-    return _ZN7android6Parcel13writeString16EPKtj();
-}
 
-/***********************************************************************************/
-// GPSD //
-/***********************************************************************************/
-
-// android::Singleton<android::SensorManager>::sLock
-extern "C" int _ZN7android9SingletonINS_13SensorManagerEE5sLockE = NULL;
-
-// android::Singleton<android::SensorManager>::sInstance
-extern "C" int _ZN7android9SingletonINS_13SensorManagerEE9sInstanceE = NULL;
-
-// android::SensorManager::SensorManager()
-extern "C" int _ZN7android13SensorManagerC1Ev();
-extern "C" int _ZN7android13SensorManagerC1Ev(){
-    return _ZN7android13SensorManagerC1Ev();
-}
-
-// android::SensorManager::createEventQueue()
-extern "C" int _ZN7android13SensorManager16createEventQueueEv();
-extern "C" int _ZN7android13SensorManager16createEventQueueEv(){
-    return _ZN7android13SensorManager16createEventQueueEv();
-}
-
-extern "C" {
-void *CRYPTO_malloc(int num, const char *file, int line) {
-    return malloc(num);
-}
+    return _ZN7android6Parcel13writeString16EPKDsj();
 
 }
-/***********************************************************************************/
-// AT_DISTRIBUTOR //
-/***********************************************************************************/
-
-extern "C" int IsSupportBrcmLogicalUart = NULL;
-extern "C" int IsSupportSwitchPhysicalUart = NULL;
-extern "C" int IsSupportQualcommLogicalUart = NULL;
-extern "C" int IsDuosRil = NULL;
 
